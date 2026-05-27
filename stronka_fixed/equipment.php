e5fecd03cb3bebd9986168b8d872d7c91f836a7f
kasacki <147598480+kasacki@users.noreply.github.com>
2026-05-27 15:38:27 +0100

    Delete stronka_fixed directory

--- stronka_fixed/equipment.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'db.php';
-$page_css = 'equipment';
-checkLogin();
-
-$equipment = $pdo->query("SELECT * FROM equipment")->fetchAll();
-
-include 'views/equipment.view.php';
--- stronka_fixed/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <p>&copy; 2026 FitLife Gym. LTW Project.</p>
-</footer>
-</body>
-</html>
